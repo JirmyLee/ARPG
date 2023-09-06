9da770689ccf5c755bac86ae055e7eb86dec9955
JirmyLee <xiaomo_lzm@163.com>
2023-09-06 20:57:32 +0800

    1.添加AI技能1后续连击和对应受伤动画表现 2.解决切换大剑后受击后会把大剑当成单手剑问题：受击时如果没有装备大剑直接返回BaseMotion；如果装备大剑右键放开则收起大剑动画；如果装备大剑右键按着则回到大剑空闲动画

--- ARPG/Assets/Scripts/AnimationEvent/CharacterWeaponAnimEvent.cs
+++ ARPG/Assets/Scripts/AnimationEvent/CharacterWeaponAnimEvent.cs
@@ -1,13 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterWeaponAnimEvent : MonoBehaviour
 {
+    protected int equipSWeaponID = Animator.StringToHash("EquipSWeapon");	//指示当前动画是否持着大刀
+    
     [SerializeField] private Transform hipGreatSowrd;   //后背大刀
     [SerializeField] private Transform handGreatSowrd;  //手上大刀
     [SerializeField] private Transform hipKatana;  //后背武士刀
     [SerializeField] private Transform handKatana;  //手上武士刀
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
 
     private void ShowGS()
     {
@@ -17,6 +26,7 @@ public class CharacterWeaponAnimEvent : MonoBehaviour
             hipKatana.gameObject.SetActive(true);
             hipGreatSowrd.gameObject.SetActive(false);
             handKatana.gameObject.SetActive(false);
+            animator.SetBool(equipSWeaponID,true);
         }
 
     }
@@ -29,7 +39,7 @@ public class CharacterWeaponAnimEvent : MonoBehaviour
             hipGreatSowrd.gameObject.SetActive(true);
             handKatana.gameObject.SetActive(true);
             hipKatana.gameObject.SetActive(false);
-            
+            animator.SetBool(equipSWeaponID,false);
         }
     }
 }
